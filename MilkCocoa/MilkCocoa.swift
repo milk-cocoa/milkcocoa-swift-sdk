990c8715dbafbd5d844dfcfe0ea17febd6526cc5
Shuhei Hiya <syuhei176@gmail.com>
2015-11-03 14:38:31 +0900

    first commit

---
+++ MilkCocoa/MilkCocoa.swift
@@ -0,0 +1,22 @@
+//
+//  MilkCocoa.swift
+//  MilkCocoa
+//
+//  Created by HIYA SHUHEI on 2015/11/03.
+//  Copyright © 2015年 Shuhei Hiya. All rights reserved.
+//
+
+import Foundation
+
+//
+//  MilkCocoa.swift
+//  MilkCocoa
+//
+//  Created by HIYA SHUHEI on 2015/05/23.
+//  Copyright (c) 2015年 Shuhei Hiya. All rights reserved.
+//
+
+import Foundation
+
+public class MilkCocoa {
+}
