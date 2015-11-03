fb5075ea54cb68acc272f69e5e8bc91d7b9eafc1
Shuhei Hiya <syuhei176@gmail.com>
2015-11-03 14:34:20 +0900

    Initial Commit

---
+++ MilkCocoa/MilkCocoa.h
@@ -0,0 +1,19 @@
+//
+//  MilkCocoa.h
+//  MilkCocoa
+//
+//  Created by HIYA SHUHEI on 2015/11/03.
+//  Copyright © 2015年 Shuhei Hiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MilkCocoa.
+FOUNDATION_EXPORT double MilkCocoaVersionNumber;
+
+//! Project version string for MilkCocoa.
+FOUNDATION_EXPORT const unsigned char MilkCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MilkCocoa/PublicHeader.h>
+
+
